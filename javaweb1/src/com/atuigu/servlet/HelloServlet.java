3199925a79bf3e2f9a30c7c1f8c0bb0447060a32
raythink <ray.thinkman@gamil.com>
2024-01-11 11:08:21 +0800

    version1.0

---
+++ javaweb1/src/com/atuigu/servlet/HelloServlet.java
@@ -0,0 +1,22 @@
+package com.atuigu.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet",urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       resp.getWriter().println("hello world");
+    }
+}
